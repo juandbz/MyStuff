bc6fdc1f10025bc92c3fee91c0697f92fafd556a
juandbz <39786806+juandbz@users.noreply.github.com>
2021-05-14 09:07:46 -0400

    Add files via upload

---
+++ Misc/vloop.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import getpass
+import sys
+import telnetlib
+
+HOST = "192.168.1.150"
+user = raw_input("Enter your remote name account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("Username: ")
+tn.write(user + "\n")
+if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
+
+tn.write("conf t\n")
+
+for n in range (2,101):
+    tn.write("vlan " + str(n) +"\n")
+    tn.write("name python_vlan_" + str(n) +"\n")
+
+tn.write("end\n")
+tn.write("exit\n")
+
+print tn.read_all()
