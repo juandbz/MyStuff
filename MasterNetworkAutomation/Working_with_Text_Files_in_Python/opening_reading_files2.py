b23a7649075708c501fe5b71f7d6a2a793d90aad
Juan Hernandez <juandbz255@gmail.com>
2021-08-21 15:53:25 -0700

    Added new file

--- MasterNetworkAutomation/Working_with_Text_Files_in_Python/opening_reading_files2.py
+++ MasterNetworkAutomation/Working_with_Text_Files_in_Python/opening_reading_files2.py
@@ -1,7 +1,17 @@
 f = open('configuration.txt', 'r')
-content = f.read()
+content = f.read(5)
 print(content)
+
+content = f.read(3)
+print(content)
+
+print(f.tell())
+
+f.seek(2)
+print(f.read(3))
+
+f.seek(0)
+print(f.read(1))
 
 print(f.closed)
 f.close()
-print(f.closed)
